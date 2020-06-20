82a366078d883961ff216cbc852b98a3f70afd8c
Takeshi Itami <tksh.itm@gmail.com>
2020-06-21 08:49:57 +0900

    add last

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -49,6 +49,7 @@ defmodule Issues.CLI do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response
     |> sort_into_descending_order
+    |> last(count)
   end
 
   def decode_response({:ok, body}), do: body
@@ -61,5 +62,11 @@ defmodule Issues.CLI do
   def sort_into_descending_order(list_of_issues) do
     list_of_issues
     |> Enum.sort(fn i1, i2 -> i1["created_at"] >= i2["created_at"] end)
+  end
+
+  def last(list, count) do
+    list
+    |> Enum.take(count)
+    |> Enum.reverse()
   end
 end
